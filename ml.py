cecef81af78e72a1f601146f9d2c5d2523e0658a
Pranav Kiruthik <pranavkiruthikmani@gmail.com>
2025-08-09 11:06:04 +0000

    Finished python api need to integrate into site

--- ml.py
+++ ml.py
@@ -1,14 +1,41 @@
-import tmdbsimple as tmdb
+import requests
 import pandas as pd
 
-tmdb.API_KEY = '24244c1d5700d01a254a2113b5bae618'
-tmdb.REQUESTS_TIMEOUT = (2, 5)  # seconds, for connect and read specifically 
-
-movie_in = input("Enter movie: ")
-search = tmdb.Search()
+HEADER = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDI0NGMxZDU3MDBkMDFhMjU0YTIxMTNiNWJhZTYxOCIsIm5iZiI6MTc1NDQ3MjUzNC43Mjk5OTk4LCJzdWIiOiI2ODkzMjA1NmQxOGI5OWEzYjVmM2YyMjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.4jecF26Vd1wII79cLjnB4DcuKSf72MEhXBr51pkWRfc"}
 
 def search_movie(movie_input):
-    response = search.movie(query=movie_input)
-    for r in response['results']:
-        return {'title': r['title'], 'id': r['id']}
-    
+    response = requests.get(f'https://api.themoviedb.org/3/search/movie?query={movie_input}', headers=HEADER).json()
+    df_raw = pd.DataFrame(response['results'])
+    df = df_raw[['id', 'title', 'popularity', 'genre_ids', 'release_date']]
+    return df
+
+def get_movie(id):
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{str(id)}', headers=HEADER).json()
+    return response
+
+def find_movies(genres):
+    if genres: #Check if the input is not empty
+        arg = ''
+        for i in genres:
+            arg += str(i)
+            arg += ','
+        arg = arg[:-1] 
+        response = requests.get(f'https://api.themoviedb.org/3/discover/movie?with_genres={arg}', headers=HEADER).json()
+        df_raw = pd.DataFrame(response['results'])
+        # print(df_raw.columns)
+        df = df_raw[['id', 'title', 'popularity', 'genre_ids']]
+        df = df.sort_values(by='popularity', ascending=False)
+        return(df)
+    else:
+        return(None)
+
+def get_genres(movie):
+    genre = []
+    genre_list = movie['genres']
+    for i in genre_list:
+        genre.append(i['id'])
+    return genre
+
+matrix = get_movie(603)
+genres = get_genres(matrix)
+print(find_movies(genres))
